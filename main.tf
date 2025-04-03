adcb05bcc9095cfa351370fc68bb67fccc3fadb1
cureoglu1 <cureoglumustafa@gmail.com>
2025-04-03 17:12:09 +0300

    network and subnets

--- main.tf
+++ main.tf
@@ -1,1 +1,19 @@
+provider "google" {
+    //credentials = key.json
+    project = "devops-study-25"
+    region  = "europe-west3"
+  
+}
 
+resource "google_compute_network" "vpc_network" {
+    name = "cure1-vpc"
+    project = "devops-study-25"
+    auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "subnetwork" {
+  name = "cure1-subnetwork"
+  ip_cidr_range = "10.156.0.0/20"
+  region = "europe-west3"
+  network = google_compute_network.vpc_network.id
+}
